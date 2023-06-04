2cd50ebecd6a3c454752e732e6cc7994cc072d05
Filomeno Ponce <110742301+filomenoPonce580@users.noreply.github.com>
2023-06-04 08:10:17 -0700

    optimize function

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-function spinWords(string){
-  const wordsArray = string.split(" ")
-  
-  let flipped = wordsArray.map((word, indx, arr) => {
-    if(word.length >= 5){
-      return word.split("").reverse().join("")
-    }else{
-      return word
-    }
-  })
-  
-  return flipped.join(" ")
+function spinWords(str){
+  return str.split(' ')
+            .map(word => word.length<5 ? word : word.split('').reverse().join('')).join(' ');
 }
+
+//FIRST ATTEMPT, FUNCTIONAL BUT NOT IDEAL
+// function spinWords(string){
+//   const wordsArray = string.split(" ")
+  
+//   let flipped = wordsArray.map((word, indx, arr) => {
+//     if(word.length >= 5){
+//       return word.split("").reverse().join("")
+//     }else{
+//       return word
+//     }
+//   })
+  
+//   return flipped.join(" ")
+// }
